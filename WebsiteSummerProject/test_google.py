65873ee0f018cec501366033bad1cb539ede45f3
kxg63330-coder <kxg63330@ucmo.edu>
2026-05-28 16:25:59 -0500

    Delete WebsiteSummerProject/test_google.py

--- WebsiteSummerProject/test_google.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-try:
-    r = requests.get("https://www.google.com")
-    print("Status:", r.status_code)
-    print("Length:", len(r.text))
-except Exception as e:
-    print("Error:", e)
