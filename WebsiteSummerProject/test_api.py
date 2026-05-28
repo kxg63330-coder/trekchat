7351d4f39cad257c8aa991271e552288ec5be876
kxg63330-coder <kxg63330@ucmo.edu>
2026-05-28 16:25:50 -0500

    Delete WebsiteSummerProject/test_api.py

--- WebsiteSummerProject/test_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-try:
-    r = requests.get("https://newsdata.io/api/1/news?apikey=pub_dae6d697dfed43c1b007d6a203faf607&country=us")
-    print("Status:", r.status_code)
-    print("Length:", len(r.text))
-    print(r.text[:300])
-except Exception as e:
-    print("Error:", e)
