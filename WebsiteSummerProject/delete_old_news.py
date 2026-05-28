262c6b82be4c0a4bbc242715ca6417761847a8ac
kxg63330-coder <kxg63330@ucmo.edu>
2026-05-28 16:23:19 -0500

    Delete WebsiteSummerProject/delete_old_news.py

--- WebsiteSummerProject/delete_old_news.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from extensions import db
-from models import News
-
-with app.app_context():
-    deleted = News.query.delete()
-    db.session.commit()
-    print(f"Deleted {deleted} old articles successfully.")
